407f450d06617f65f3bd67638e0176f57591c4c5
USER <USER@WIN-QGII8ID360C>
2022-08-13 17:18:04 +0500

    Добавьте файлы проекта.

---
+++ Joqaridan_harip_tus/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Joqaridan_harip_tus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.baslaw = new Guna.UI2.WinForms.Guna2Button();
+            this.waqit = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.Green;
+            this.guna2Panel1.Location = new System.Drawing.Point(1, 632);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(1117, 100);
+            this.guna2Panel1.TabIndex = 0;
+            this.guna2Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.guna2Panel1_Paint);
+            // 
+            // baslaw
+            // 
+            this.baslaw.BackColor = System.Drawing.Color.DarkMagenta;
+            this.baslaw.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.baslaw.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.baslaw.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.baslaw.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.baslaw.FillColor = System.Drawing.Color.Purple;
+            this.baslaw.Font = new System.Drawing.Font("Segoe UI Semilight", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.baslaw.ForeColor = System.Drawing.Color.Yellow;
+            this.baslaw.Location = new System.Drawing.Point(163, 137);
+            this.baslaw.Name = "baslaw";
+            this.baslaw.Size = new System.Drawing.Size(192, 62);
+            this.baslaw.TabIndex = 0;
+            this.baslaw.Text = "Baslaw";
+            this.baslaw.Visible = false;
+            this.baslaw.Click += new System.EventHandler(this.guna2Button1_Click);
+            this.baslaw.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.guna2Button1_KeyPress);
+            // 
+            // waqit
+            // 
+            this.waqit.Interval = 10;
+            this.waqit.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(42, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 65);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "a";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.guna2Button3);
+            this.panel1.Controls.Add(this.guna2Button2);
+            this.panel1.Controls.Add(this.baslaw);
+            this.panel1.Location = new System.Drawing.Point(300, 110);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(518, 351);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(16, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(499, 78);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "O\'zin\'izge qolay bolg\'an alfabitti tan\'lan\'";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // guna2Button3
+            // 
+            this.guna2Button3.BackColor = System.Drawing.Color.DarkMagenta;
+            this.guna2Button3.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button3.FillColor = System.Drawing.Color.Purple;
+            this.guna2Button3.Font = new System.Drawing.Font("Segoe UI Semilight", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.guna2Button3.ForeColor = System.Drawing.Color.Yellow;
+            this.guna2Button3.Location = new System.Drawing.Point(163, 42);
+            this.guna2Button3.Name = "guna2Button3";
+            this.guna2Button3.Size = new System.Drawing.Size(192, 62);
+            this.guna2Button3.TabIndex = 2;
+            this.guna2Button3.Text = "Latin";
+            this.guna2Button3.Click += new System.EventHandler(this.guna2Button3_Click);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.BackColor = System.Drawing.Color.DarkMagenta;
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.Purple;
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI Semilight", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.guna2Button2.ForeColor = System.Drawing.Color.Yellow;
+            this.guna2Button2.Location = new System.Drawing.Point(163, 137);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(192, 62);
+            this.guna2Button2.TabIndex = 1;
+            this.guna2Button2.Text = "Кирилл";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1119, 728);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2Panel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Button baslaw;
+        private System.Windows.Forms.Timer waqit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Joqaridan_harip_tus/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using WMPLib;
+
+namespace Joqaridan_harip_tus
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer player=new WindowsMediaPlayer();
+        WindowsMediaPlayer player2=new WindowsMediaPlayer();
+        WindowsMediaPlayer player3=new WindowsMediaPlayer();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        Random xr=new Random();
+        Random yr=new Random();
+        Random yk=new Random();
+        char b = 'f';
+        int x=0,y=0,sekund=0,latin=0,kirill=0,duris=0;
+        private void manis_beriw()
+        {
+            x=xr.Next(0,700);
+            label1.Location=new Point(x,0);
+            if(latin==1&&kirill==0)
+            b = (char)yr.Next('a', 'z');
+            else
+                b = (char)yr.Next('а', 'я');
+            label1.Text = b.ToString();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sekund++;
+           
+
+
+            y += 1;
+                label1.Location = new Point(x, y);
+            
+           
+            if(label1.Location.Y>405)
+            {
+                player2.URL = @"zvonok.wav";
+                manis_beriw();
+                y = 0;
+            }
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            player.URL = @"tanaka.Mp3";
+            panel1.Visible=false;
+            manis_beriw();
+            label1.Visible = true;
+            waqit.Start();
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            latin++;
+            koriniw();
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            kirill++;
+            koriniw();
+        }
+        private void koriniw()
+        {
+            guna2Button3.Visible = false;
+            guna2Button2.Visible = false;
+            baslaw.Visible = true;
+            label2.Visible = false;
+        }
+        private void guna2Button1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString() == label1.Text)
+            {
+                duris++;
+                manis_beriw();
+                y = 0;
+                
+
+            }
+            else
+            {
+                player3.URL = @"wrong.wav";
+
+            }
+        }
+
+        private void guna2Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void a_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
